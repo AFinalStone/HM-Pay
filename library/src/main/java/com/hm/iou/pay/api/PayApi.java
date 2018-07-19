9cf23f8a631c7fd3d3e2876c5f28a506d233447e
shilei <shiyaolei@dafy.com>
2018-07-19 14:57:05 +0800

    添加接口说明

--- library/src/main/java/com/hm/iou/pay/api/PayApi.java
+++ library/src/main/java/com/hm/iou/pay/api/PayApi.java
@@ -4,6 +4,8 @@ import com.hm.iou.network.HttpReqManager;
 import com.hm.iou.pay.bean.HistoryItemBean;
 import com.hm.iou.pay.bean.PayTestBean;
 import com.hm.iou.pay.bean.SearchTimeCardListResBean;
+import com.hm.iou.pay.bean.req.ChannelEnumReqBean;
+import com.hm.iou.pay.bean.req.CreatePreparePayReqBean;
 import com.hm.iou.sharedata.model.BaseResponse;
 
 import java.util.List;
@@ -21,16 +23,65 @@ public class PayApi {
         return HttpReqManager.getInstance().getService(PayService.class);
     }
 
+    /**
+     * 微信支付测试接口
+     *
+     * @return
+     */
     public static Flowable<PayTestBean> payTest() {
         return getService().payTest().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
     }
 
+    /**
+     * 获取充值消费历史记录
+     *
+     * @return
+     */
     public static Flowable<BaseResponse<List<HistoryItemBean>>> getHistory() {
         return getService().getHistory().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
     }
 
+    /**
+     * 搜索次卡套餐列表
+     *
+     * @return
+     */
     public static Flowable<BaseResponse<SearchTimeCardListResBean>> searchTimeCardPackageList() {
         return getService().searchTimeCardPackageList().subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+    }
+
+    /**
+     * 创建订单
+     *
+     * @param packageId
+     * @return
+     */
+    public static Flowable<BaseResponse<String>> createOrder(String packageId) {
+        return getService().createOrder(packageId).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+    }
+
+    /**
+     * 通过订单id查询订单状态
+     *
+     * @param orderId
+     * @return
+     */
+    public static Flowable<BaseResponse<String>> queryOrderPayState(String orderId) {
+        return getService().queryOrderPayState(orderId).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+    }
+
+    /**
+     * 创建预付款订单
+     *
+     * @param chanel  平台 1微信支付 2苹果支付
+     * @param orderId
+     * @return
+     */
+    public static Flowable<BaseResponse<PayTestBean>> createPreparePayOrder(ChannelEnumReqBean chanel, String orderId) {
+        CreatePreparePayReqBean reqBean = new CreatePreparePayReqBean();
+        reqBean.setChannel(chanel.getValue());
+        reqBean.setOrderId(orderId);
+        return getService().createPreparePayOrder(reqBean).subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
     }
 
 }
--- library/src/main/java/com/hm/iou/pay/api/PayService.java
+++ library/src/main/java/com/hm/iou/pay/api/PayService.java
@@ -3,12 +3,15 @@ package com.hm.iou.pay.api;
 import com.hm.iou.pay.bean.HistoryItemBean;
 import com.hm.iou.pay.bean.PayTestBean;
 import com.hm.iou.pay.bean.SearchTimeCardListResBean;
+import com.hm.iou.pay.bean.req.CreatePreparePayReqBean;
 import com.hm.iou.sharedata.model.BaseResponse;
 
 import java.util.List;
 
 import io.reactivex.Flowable;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 /**
  * Created by syl on 2018/7/12.
@@ -26,12 +29,12 @@ public interface PayService {
     Flowable<BaseResponse<SearchTimeCardListResBean>> searchTimeCardPackageList();
 
     @GET("/pay/iou/v1/createOrder")
-    Flowable<BaseResponse<String>> createOrder();
+    Flowable<BaseResponse<String>> createOrder(@Query("packageId") String packageId);
 
     @GET("/pay/iou/v1/queryOrderWhilePaying")
-    Flowable<BaseResponse<String>> queryOrderPayState();
+    Flowable<BaseResponse<String>> queryOrderPayState(@Query("orderId") String orderId);
 
     @GET("/pay/iou/v1/unifiedOrder")
-    Flowable<BaseResponse<PayTestBean>> createPreparePayOrder();
+    Flowable<BaseResponse<PayTestBean>> createPreparePayOrder(@Body CreatePreparePayReqBean reqBean);
 
 }
