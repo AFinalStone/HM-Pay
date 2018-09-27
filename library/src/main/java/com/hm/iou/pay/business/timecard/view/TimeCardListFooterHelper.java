7c52e06d27ef674d20b1255fe8f16e8fa3324406
石磊 <syl@shileideMacBook-Pro.local>
2018-09-27 13:42:41 +0800

    修改广告的样式

--- library/src/main/java/com/hm/iou/pay/business/timecard/view/TimeCardListFooterHelper.java
+++ library/src/main/java/com/hm/iou/pay/business/timecard/view/TimeCardListFooterHelper.java
@@ -13,6 +13,7 @@ import com.hm.iou.pay.R;
 import com.hm.iou.pay.business.timecard.TimeCardRechargeContract;
 import com.hm.iou.pay.comm.ITimeCardItem;
 import com.hm.iou.router.Router;
+import com.hm.iou.tools.DensityUtil;
 import com.hm.iou.tools.ImageLoader;
 
 /**
@@ -31,6 +32,12 @@ public class TimeCardListFooterHelper {
         mFooterView = LayoutInflater.from(mContext).inflate(R.layout.pay_item_time_card_recharge_footer, parentView, false);
         mTvFirstTry = mFooterView.findViewById(R.id.tv_firstTry);
         mIvAdvertisement = mFooterView.findViewById(R.id.iv_advertisement);
+        float width = mContext.getResources().getDisplayMetrics().widthPixels - DensityUtil.dip2px(mContext, 20);
+        float height = width * 5 / 12;
+        ViewGroup.LayoutParams layoutParams = mIvAdvertisement.getLayoutParams();
+        layoutParams.width = (int) width;
+        layoutParams.height = (int) height;
+        mIvAdvertisement.setLayoutParams(layoutParams);
     }
 
     public View getFooterView() {
