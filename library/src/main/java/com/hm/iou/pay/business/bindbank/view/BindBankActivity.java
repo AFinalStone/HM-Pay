cadcf0bbb861a0ec41124159a096b8bc1bde99ab
石磊 <>
2019-01-17 14:25:38 +0800

    修改绑定银行卡的弹窗IOSAlert为HMAlert

--- library/src/main/java/com/hm/iou/pay/business/bindbank/view/BindBankActivity.java
+++ library/src/main/java/com/hm/iou/pay/business/bindbank/view/BindBankActivity.java
@@ -1,7 +1,5 @@
 package com.hm.iou.pay.business.bindbank.view;
 
-import android.app.Dialog;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.Gravity;
@@ -10,7 +8,7 @@ import com.hm.iou.base.BaseActivity;
 import com.hm.iou.pay.business.bindbank.BindBinkContract;
 import com.hm.iou.pay.business.bindbank.presenter.BindBankPresenter;
 import com.hm.iou.router.Router;
-import com.hm.iou.uikit.dialog.IOSAlertDialog;
+import com.hm.iou.uikit.dialog.HMAlertDialog;
 
 /**
  * @author syl
@@ -35,46 +33,59 @@ public class BindBankActivity extends BaseActivity<BindBankPresenter> implements
 
     @Override
     public void warnNoTimeToBindToday() {
-        new IOSAlertDialog.Builder(mContext)
-                .setTitle("管家提醒")
-                .setMessage("银行卡或预留银行的手机号有误。每日三次银行卡认证已达上限。")
-                .setPositiveButton("明日再试", new DialogInterface.OnClickListener() {
+        String title = "管家提醒";
+        String msg = "银行卡或预留银行的手机号有误。每日三次银行卡认证已达上限。";
+        new HMAlertDialog
+                .Builder(mContext)
+                .setTitle(title)
+                .setMessage(msg)
+                .setPositiveButton("明日再试")
+                .setCancelable(false)
+                .setCanceledOnTouchOutside(false)
+                .setOnClickListener(new HMAlertDialog.OnClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
+                    public void onPosClick() {
                         finish();
                     }
-                }).show();
+
+                    @Override
+                    public void onNegClick() {
+                        finish();
+                    }
+                })
+                .create()
+                .show();
     }
 
     @Override
     public void showAuthDialog() {
-        Dialog dialog = new IOSAlertDialog.Builder(mContext)
-                .setTitle("银行卡认证")
-                .setMessage("通过银行卡认证，您可以免费获得1次签章的机会，目前您未通过实名认证，是否立即认证实名信息？")
-                .setGravity(Gravity.LEFT)
-                .setNegativeButton("以后再说", new DialogInterface.OnClickListener() {
+        String title = "银行卡认证";
+        String msg = "通过银行卡认证，您可以免费获得1次签章的机会，目前您未通过实名认证，是否立即认证实名信息？";
+        new HMAlertDialog
+                .Builder(mContext)
+                .setTitle(title)
+                .setMessage(msg)
+                .setMessageGravity(Gravity.LEFT)
+                .setNegativeButton("以后再说")
+                .setPositiveButton("立即认证")
+                .setCancelable(false)
+                .setCanceledOnTouchOutside(false)
+                .setOnClickListener(new HMAlertDialog.OnClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                })
-                .setPositiveButton("立即认证", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+                    public void onPosClick() {
                         Router.getInstance()
                                 .buildWithUrl("hmiou://m.54jietiao.com/facecheck/authentication")
                                 .navigation(mContext);
-                        dialog.dismiss();
+                        finish();
+                    }
+
+                    @Override
+                    public void onNegClick() {
+                        finish();
                     }
                 })
+                .create()
                 .show();
-        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-            @Override
-            public void onDismiss(DialogInterface dialog) {
-                finish();
-            }
-        });
     }
 
     @Override
@@ -101,22 +112,26 @@ public class BindBankActivity extends BaseActivity<BindBankPresenter> implements
         } else {
             sbMsg.append("），手机号尾号" + phoneCode + "，如需更换信息，请联系客服，服务费¥2。");
         }
-        Dialog dialog = new IOSAlertDialog.Builder(mContext)
+        new HMAlertDialog
+                .Builder(mContext)
                 .setTitle("银行卡认证")
                 .setMessage(sbMsg.toString())
-                .setGravity(Gravity.LEFT)
-                .setNegativeButton("知道了", new DialogInterface.OnClickListener() {
+                .setMessageGravity(Gravity.LEFT)
+                .setNegativeButton("知道了")
+                .setCancelable(false)
+                .setCanceledOnTouchOutside(false)
+                .setOnClickListener(new HMAlertDialog.OnClickListener() {
                     @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
+                    public void onPosClick() {
+                        finish();
+                    }
+
+                    @Override
+                    public void onNegClick() {
+                        finish();
                     }
                 })
+                .create()
                 .show();
-        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
-            @Override
-            public void onDismiss(DialogInterface dialog) {
-                finish();
-            }
-        });
     }
 }
