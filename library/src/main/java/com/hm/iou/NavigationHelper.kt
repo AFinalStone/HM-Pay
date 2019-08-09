b67d56770ba2dd1e2e03b428a7b52b6049df5a6e
石磊 <>
2019-08-09 11:11:26 +0800

    解决BUG

--- library/src/main/java/com/hm/iou/NavigationHelper.kt
+++ library/src/main/java/com/hm/iou/NavigationHelper.kt
@@ -3,17 +3,24 @@
 package com.hm.iou
 
 import android.content.Context
+import android.content.Intent
 import com.google.gson.Gson
 import com.hm.iou.pay.bean.VipCardPackageBean
 import com.hm.iou.pay.business.timecard.view.VipCardPackageDetailActivity
-import com.hm.iou.tools.kt.startActivity
 
 /**
  * 贵宾卡支付页面
  */
 fun toVipCardPayPage(context: Context, vipCard: VipCardPackageBean) {
-    val vipCardPackageBeanJson = Gson().toJson(vipCard)
-    context.startActivity<VipCardPackageDetailActivity>(
-            VipCardPackageDetailActivity.EXTRA_KEY_PACKAGE_INFO_JSON to vipCardPackageBeanJson
-    )
+    var vipCardJson: String? = null
+    try {
+        vipCardJson = Gson().toJson(vipCard)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    vipCardJson?.let {
+        val intent = Intent(context, VipCardPackageDetailActivity::class.java)
+        intent.putExtra(VipCardPackageDetailActivity.EXTRA_KEY_PACKAGE_INFO_JSON, vipCardJson)
+        context.startActivity(intent)
+    }
 }
